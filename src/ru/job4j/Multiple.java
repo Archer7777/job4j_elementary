79658f6d57305eceb287d332a6ef49975751e4c0
alekseypudovkin <freem4n382@yandex.ru>
2021-02-04 21:45:00 +0300

    3.4. Заголовок в файле java [#253313]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -4,6 +4,6 @@ public class Multiple {
     public static void main(String[] args) {
         System.out.println("1*2=2");
         System.out.println("1*3=3");
-        System.out.println("1*9=");
+        System.out.println("1*9=9");
     }
 }
