a10437d0f052927b1207f13d37d1eae9c5f79d4c
alekseypudovkin <freem4n382@yandex.ru>
2021-02-13 10:34:22 +0300

    9.1. String [#466660]

---
+++ src/ru/job4j/condtition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condtition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        int year = 2021;
+        String idea = "I like Java!" + year;
+        String but = " But I am a newbie.";
+        System.out.println(idea + but);
+        // ИЛИ так)
+        System.out.println(idea + " But I am a newbie.");
+        System.out.println(idea);
+
+
+    }
+}
