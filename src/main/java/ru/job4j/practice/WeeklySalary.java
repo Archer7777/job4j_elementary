0cf4755b407b5b3b3edfa7fed81ef651db57c2c4
Aleksey <freem4n382@yandex.ru>
2021-11-19 20:31:23 +0300

    Задача 1.6

---
+++ src/main/java/ru/job4j/practice/WeeklySalary.java
@@ -0,0 +1,29 @@
+package ru.job4j.practice;
+
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int regular = 10;
+        int extra = 15;
+        //int doubleRegular = 20; // странноа
+        int doubleExtra = 30;
+        int salary = 0;
+
+        for (int i = 0; i < hours.length; i++)
+            if (i < 5) {
+                if (hours[i] <= 8) {
+                    salary = salary + hours[i] * regular;
+                }
+                else {
+                    salary = salary + (hours[i] - 8) * extra + 8 * regular;
+                }
+            } else {
+                if (hours[i] <= 8) {
+                    salary = salary + hours[i] * extra;
+                }
+                else {
+                    salary = salary + (hours[i] - 8) * doubleExtra + hours[i] * extra;
+                }
+            }
+        return salary;
+    }
+}
